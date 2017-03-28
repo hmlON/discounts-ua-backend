2f491302d1d0ba44af2ac0c151bb3f51c1c1d31a
Nikita Kholin <nik.kholin@gmail.com>
2017-03-28 23:04:28 +0300

    Add parsing of other pages of silpo#priceoftheweek

--- websites/silpo.rb
+++ websites/silpo.rb
@@ -9,13 +9,17 @@ class Silpo
   def price_of_the_week
     page = Nokogiri::HTML(open(base_uri + '/ru/actions/priceoftheweek'))
     discounts = []
-    page.css('.ots .photo').each do |discount|
-      discounts << { name: discount.css('.img h3').first.text,
-                     img_url: base_uri + discount.css('.img .pirobox').first.attributes['href'].value,
-                     price_new: discount.css('.price_2014_new .hrn').first.text + '.' +
-                                discount.css('.price_2014_new .kop').first.text,
-                     price_old: discount.css('.price_2014_old .hrn').first.text + '.' +
-                                discount.css('.price_2014_old .kop').first.text }
+    pages_count = page.css('.ots .page div').count
+    1.upto(pages_count) do |i|
+      page = Nokogiri::HTML(open(base_uri + "/ru/actions/priceoftheweek/?PAGEN_1=#{i}"))
+      page.css('.ots .photo').each do |discount|
+        discounts << { name: discount.css('.img h3').first.text,
+                       img_url: base_uri + discount.css('.img .pirobox').first.attributes['href'].value,
+                       price_new: discount.css('.price_2014_new .hrn').first.text + '.' +
+                                  discount.css('.price_2014_new .kop').first.text,
+                       price_old: discount.css('.price_2014_old .hrn').first.text + '.' +
+                                  discount.css('.price_2014_old .kop').first.text }
+      end
     end
     discounts
   end
