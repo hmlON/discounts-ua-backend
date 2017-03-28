c7badf67367820d74f6b796f2021ab769a377c7e
Nikita Kholin <nik.kholin@gmail.com>
2017-03-28 23:49:49 +0300

    Extract page parsing into method

--- websites/silpo.rb
+++ websites/silpo.rb
@@ -2,31 +2,49 @@ require 'nokogiri'
 require 'open-uri'
 
 class Silpo
-  def discounts
-    price_of_the_week
+  attr_reader :discounts
+
+  def initialize
+    @discounts = price_of_the_week
   end
 
   def price_of_the_week
-    page = Nokogiri::HTML(open(base_uri + '/ru/actions/priceoftheweek'))
+    page = Nokogiri::HTML(open(BASE_URL + 'priceoftheweek'))
     discounts = []
     pages_count = page.css('.ots .page div').count
     1.upto(pages_count) do |i|
-      page = Nokogiri::HTML(open(base_uri + "/ru/actions/priceoftheweek/?PAGEN_1=#{i}"))
-      page.css('.ots .photo').each do |discount|
-        discounts << { name: discount.css('.img h3').first.text,
-                       img_url: base_uri + discount.css('.img .pirobox').first.attributes['href'].value,
-                       price_new: discount.css('.price_2014_new .hrn').first.text + '.' +
-                                  discount.css('.price_2014_new .kop').first.text,
-                       price_old: discount.css('.price_2014_old .hrn').first.text + '.' +
-                                  discount.css('.price_2014_old .kop').first.text }
-      end
+      discounts += parse_page("priceoftheweek/?PAGEN_1=#{i}")
     end
     discounts
   end
 
   private
 
-  def base_uri
-    'http://silpo.ua'
+  BASE_URL = 'http://silpo.ua/ru/actions/'
+  BASE_IMG_URL = 'http://silpo.ua/'
+
+  DEFAULT_OPTIONS = {
+    all_discounts_css: '.ots .photo',
+    name_css: '.img h3',
+    img_url_css: '.img .pirobox',
+    price_new_css: { hrn: '.price_2014_new .hrn',
+                     kop: '.price_2014_new .kop' },
+    price_old_css: { hrn: '.price_2014_old .hrn',
+                     kop: '.price_2014_old .kop' }
+  }
+
+  def parse_page(url, options = {})
+    options = DEFAULT_OPTIONS.merge(options)
+    page_discounts = []
+    page = Nokogiri::HTML(open(BASE_URL + url))
+    page.css(options[:all_discounts_css]).each do |discount|
+      page_discounts << { name: discount.css(options[:name_css]).first.text,
+        img_url: BASE_IMG_URL + discount.css(options[:img_url_css]).first.attributes['href'].value,
+        price_new: discount.css(options[:price_new_css][:hrn]).first.text + '.' +
+                   discount.css(options[:price_new_css][:kop]).first.text,
+        price_old: discount.css(options[:price_old_css][:hrn]).first.text + '.' +
+                   discount.css(options[:price_old_css][:kop]).first.text }
+    end
+    page_discounts
   end
 end
