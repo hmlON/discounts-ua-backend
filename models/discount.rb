729c1d77e7eb0ef8c25eb195a0b1b5133b40de33
Nikita Kholin <nik.kholin@gmail.com>
2017-04-02 13:58:20 +0300

    Add discount schema to model description

--- models/discount.rb
+++ models/discount.rb
@@ -1,2 +1,13 @@
+# Schema
+# t.string   "name",          null: false
+# t.string   "img_url",       null: false
+# t.integer  "price_new",     null: false
+# t.integer  "price_old",     null: false
+# t.datetime "start_date"
+# t.datetime "end_date"
+# t.string   "shop_name",     null: false
+# t.string   "discount_type", null: false
+# t.datetime "created_at",    null: false
+# t.datetime "updated_at",    null: false
 class Discount < ActiveRecord::Base
 end
