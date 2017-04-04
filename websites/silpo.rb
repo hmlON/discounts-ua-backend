d2be2167f4090158eedc817746db278dea0f28f9
Nikita Kholin <nik.kholin@gmail.com>
2017-04-04 23:35:18 +0300

    Update silpo discounts parsing to use discount type periods

--- websites/silpo.rb
+++ websites/silpo.rb
@@ -2,49 +2,47 @@ require 'nokogiri'
 require 'open-uri'
 
 class Silpo
-  attr_reader :discounts
+  attr_reader :discounts, :shop
 
   def initialize
     @discounts = price_of_the_week + hot_proposal
   end
 
+  def shop
+    @shop ||= Shop.find_by(name: 'silpo')
+  end
+
   def price_of_the_week
-    page = Nokogiri::HTML(open(BASE_DISCOUNTS_URL + 'priceoftheweek'))
+    discount_type = shop.discount_types.find_by(name: 'price_of_the_week')
+
+    page = Nokogiri::HTML(open(shop.base_url + discount_type.url))
     dates = page.css('.ots').children[3].children[3].children[0].text
                 .scan(/\d{1,2}\.\d{1,2}\.\d{4}/)
                 .map { |date| Date.parse(date) }
 
-    options = {
-      start_date: dates.min,
-      end_date: dates.max,
-      discount_type: 'price_of_the_week'
-    }
-    parse_discount_type('priceoftheweek', options)
+    active_discount_type = discount_type.periods.create(start_date: dates.min, end_date: dates.max)
+    parse_discount_type(active_discount_type)
   end
 
   def hot_proposal
-    page = Nokogiri::HTML(open(BASE_DISCOUNTS_URL + 'hotproposal'))
+    discount_type = shop.discount_types.find_by(name: 'hot_proposal')
+
+    page = Nokogiri::HTML(open(shop.base_url + discount_type.url))
     dates = page.css('.ots').children[1].children[1].text
                 .scan(/\d{1,2}\.\d{1,2}\.\d{4}/)
                 .map { |date| Date.parse(date) }
 
+    active_discount_type = discount_type.periods.create(start_date: dates.min, end_date: dates.max)
     options = {
       price_new_css: { hrn: '.price-hot-new .hrn',
                        kop: '.price-hot-new .kop' },
       price_old_css: { hrn: '.price-hot-old .hrn',
-                       kop: '.price-hot-old .kop' },
-      start_date: dates.min,
-      end_date: dates.max,
-      discount_type: 'hot_proposal'
+                       kop: '.price-hot-old .kop' }
     }
-    parse_discount_type('hotproposal', options)
+    parse_discount_type(active_discount_type, options)
   end
 
   private
-
-  BASE_DISCOUNTS_URL = 'http://silpo.ua/ru/actions/'
-  BASE_URL = 'http://silpo.ua/'
-  SHOP_NAME = 'silpo'
 
   DEFAULT_OPTIONS = {
     all_discounts_css: '.ots .photo',
@@ -56,37 +54,28 @@ class Silpo
                      kop: '.price_2014_old .kop' }
   }
 
-  def parse_discount_type(url, options = {})
-    discounts = []
-    page = Nokogiri::HTML(open(BASE_DISCOUNTS_URL + url))
+  def parse_discount_type(active_period, options = {})
+    url = shop.base_url + active_period.discount_type.url
+    page = Nokogiri::HTML(open(url))
 
     pages_count = page.css('.ots .page div').count
     1.upto(pages_count) do |i|
-      discounts += parse_page("#{BASE_DISCOUNTS_URL + url}/?PAGEN_1=#{i}", options)
+      parse_page("#{url}/?PAGEN_1=#{i}", active_period, options)
     end
-
-    discounts
   end
 
-  def parse_page(url, options = {})
+  def parse_page(url, active_period, options = {})
     options = DEFAULT_OPTIONS.merge(options)
-    page_discounts = []
     page = Nokogiri::HTML(open(url))
     page.css(options[:all_discounts_css]).each do |discount|
-      Discount.find_or_create_by(
+      active_period.discounts.create(
         name: discount.css(options[:name_css]).first.text,
-        img_url: BASE_URL + discount.css(options[:img_url_css]).first.attributes['href'].value,
+        img_url: shop.base_url + discount.css(options[:img_url_css]).first.attributes['href'].value,
         price_new: discount.css(options[:price_new_css][:hrn]).first.text + '.' +
                    discount.css(options[:price_new_css][:kop]).first.text,
         price_old: discount.css(options[:price_old_css][:hrn]).first.text + '.' +
                    discount.css(options[:price_old_css][:kop]).first.text,
-        shop_name: SHOP_NAME,
-        discount_type: options[:discount_type],
-        start_date: options[:start_date],
-        end_date: options[:end_date]
       )
     end
-    # p page_discounts
-    page_discounts
   end
 end
