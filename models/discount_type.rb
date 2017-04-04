213950df63d0ba57b2c5b4eccb29638a2f5faf4f
Nikita Kholin <nik.kholin@gmail.com>
2017-04-04 23:33:52 +0300

    Add active period scope to discount type

--- models/discount_type.rb
+++ models/discount_type.rb
@@ -2,5 +2,9 @@
 # e.g. Silpo has "price_of_the_week" and "hot_proposal"
 class DiscountType < ActiveRecord::Base
   belongs_to :shop
-  has_many :discount_type_periods, dependent: :destroy
+  has_many :periods, class_name: 'DiscountTypePeriod', foreign_key: 'discount_type_id', dependent: :destroy
+
+  def active_period
+    periods.find_by('end_date >= ?', Date.today)
+  end
 end
