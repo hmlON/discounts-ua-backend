1db993ad068c4184f2926cb864d1ca3e955ea341
Nikita Kholin <nik.kholin@gmail.com>
2017-04-10 01:28:54 +0300

    Add homepage spec

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,6 +1,17 @@
-RSpec.describe "Application" do
-  it "should allow accessing the home page" do
-    get '/'
-    expect(last_response).to be_ok
+RSpec.describe "Application", type: :feature do
+  it "should display all discounts on the homepage" do
+    shop = Fabricate(:shop)
+
+    visit '/'
+
+    expect(page).to have_content shop.name.titleize
+    shop.discount_types.each do |discount_type|
+      expect(page).to have_content discount_type.name.humanize
+      expect(page).to have_content discount_type.active_period.end_date.strftime('%d %b %Y')
+
+      discount_type.active_period.discounts.each do |discount|
+        expect(page).to have_content discount.name
+      end
+    end
   end
 end
