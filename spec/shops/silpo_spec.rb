09aca2b973153217d5cbcdc276b38e8d34bd00ba
Nikita Kholin <nik.kholin@gmail.com>
2017-04-14 22:25:52 +0300

    Add specs for parsing of silpos discount types

---
+++ spec/shops/silpo_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe Silpo do
+  before :each do
+    load "#{app.settings.root}/db/seeds.rb"
+  end
+
+  describe '.price_of_the_week' do
+    it 'parses' do
+      VCR.use_cassette('silpo_price_of_the_week') do
+        expect { Silpo.new.price_of_the_week }.to change { Discount.count }
+      end
+    end
+  end
+
+  describe '.hot_proposal' do
+    it 'parses' do
+      VCR.use_cassette('silpo_hot_proposal') do
+        expect { Silpo.new.price_of_the_week }.to change { Discount.count }
+      end
+    end
+  end
+end
