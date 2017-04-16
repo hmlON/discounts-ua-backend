fd1d129653a963c4cabd6d3095077bb0ac153c77
Nikita Kholin <nik.kholin@gmail.com>
2017-04-16 18:42:43 +0300

    Extract dates parsing

--- shops/silpo/hot_proposal.rb
+++ shops/silpo/hot_proposal.rb
@@ -5,9 +5,7 @@ module Silpo
         discount_type = shop.discount_types.find_or_create_by(name: 'hot_proposal', url: '/ru/actions/hotproposal')
 
         page = Nokogiri::HTML(open(shop.base_url + discount_type.url))
-        dates = page.css('.ots').children[1].children[1].text
-                    .scan(/\d{1,2}\.\d{1,2}\.\d{4}/)
-                    .map { |date| Date.parse(date) }
+        dates = parse_dates(page)
 
         active_discount_type = discount_type.periods.create(start_date: dates.min, end_date: dates.max)
         parse_discount_type(active_discount_type)
@@ -22,6 +20,13 @@ module Silpo
           price_old_css: { hrn: '.price-hot-old .hrn',
                            kop: '.price-hot-old .kop' }
         }
+      end
+
+      # TODO: change to use ranges
+      def parse_dates(page)
+        page.css('.ots').children[1].children[1].text
+            .scan(/\d{1,2}\.\d{1,2}\.\d{4}/)
+            .map { |date| Date.parse(date) }
       end
     end
   end
--- shops/silpo/price_of_the_week.rb
+++ shops/silpo/price_of_the_week.rb
@@ -5,9 +5,7 @@ module Silpo
         discount_type = shop.discount_types.find_or_create_by(name: 'price_of_the_week', url: '/ru/actions/priceoftheweek')
 
         page = Nokogiri::HTML(open(shop.base_url + discount_type.url))
-        dates = page.css('.ots').children[3].children[3].children[0].text
-                    .scan(/\d{1,2}\.\d{1,2}\.\d{4}/)
-                    .map { |date| Date.parse(date) }
+        dates = parse_dates(page)
 
         active_discount_type = discount_type.periods.find_or_create_by(start_date: dates.min, end_date: dates.max)
         parse_discount_type(active_discount_type)
@@ -22,6 +20,13 @@ module Silpo
           price_old_css: { hrn: '.price_2014_old .hrn',
                            kop: '.price_2014_old .kop' }
         }
+      end
+
+      # TODO: change to use ranges
+      def parse_dates(page)
+        page.css('.ots').children[3].children[3].children[0].text
+            .scan(/\d{1,2}\.\d{1,2}\.\d{4}/)
+            .map { |date| Date.parse(date) }
       end
     end
   end
