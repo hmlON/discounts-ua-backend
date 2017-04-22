48344495dd214d4d7be479622de444bcf0fb4518
Nikita Kholin <nik.kholin@gmail.com>
2017-04-22 17:56:33 +0300

    Add small_img_url to discount fabricator

--- spec/fabricators/discount_fabricator.rb
+++ spec/fabricators/discount_fabricator.rb
@@ -1,6 +1,7 @@
 Fabricator(:discount) do
   discount_type_period
   name { Faker::Commerce.product_name }
+  small_img_url { Faker::LoremPixel.image }
   img_url { Faker::LoremPixel.image }
   price_new { Faker::Commerce.price }
   price_old { Faker::Commerce.price + Random.rand(Faker::Commerce.price) }
