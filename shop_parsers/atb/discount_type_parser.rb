12b8fb9ac3b6c56bf8561fe340b39025cbc31251
Nikita Kholin <nik.kholin@gmail.com>
2017-06-05 17:00:47 +0300

    Remove copy-pasted parse_discounts atb method

--- shop_parsers/atb/discount_type_parser.rb
+++ shop_parsers/atb/discount_type_parser.rb
@@ -12,12 +12,6 @@ module ATB
       def parse_discounts
         parse_page(discount_type.url)
       end
-
-      def parse_dates(page)
-        page.css('.ots').children[3].children[3].children[0].text
-            .scan(/\d{1,2}\.\d{1,2}\.\d{4}/)
-            .map { |date| Date.parse(date) }
-      end
     end
   end
 end
