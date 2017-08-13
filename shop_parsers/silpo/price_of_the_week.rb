18c6fc524a286d40387d095c063660839251654d
Nikita Kholin <nik.kholin@gmail.com>
2017-08-13 12:14:53 +0300

    Simplify price of the week date parsing

--- shop_parsers/silpo/price_of_the_week.rb
+++ shop_parsers/silpo/price_of_the_week.rb
@@ -18,11 +18,10 @@ module Silpo
         }
       end
 
-      def parse_dates(page)
-        dates = page.css('.ots').children[3].children[3].children[0].text
-                    .scan(/\d{1,2}\.\d{1,2}\.\d{4}/)
-                    .map { |date| Date.parse(date) }
-        dates.min..dates.max
+      def parse_dates(_page)
+        start_date = Date.today.beginning_of_week(:thursday)
+        end_date = start_date + 6.days
+        start_date..end_date
       end
     end
   end
