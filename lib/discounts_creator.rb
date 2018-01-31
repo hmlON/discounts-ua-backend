e4168a32099b5199d4d1d2db0fe428503ba9ab10
Nikita Kholin <nik.kholin@gmail.com>
2018-01-31 23:57:54 +0200

    Add parsing of discounts at the start of application

--- lib/discounts_creator.rb
+++ lib/discounts_creator.rb
@@ -8,12 +8,27 @@ class DiscountsCreator
   end
 
   def call
-    p current_period
-    # p discount_type
-    # discount_type_parser.class
+    discounts_data = discount_type_parser.call
+    discounts_data.map { |discount_data| find_or_create_discount(discount_data) }
+  end
+
+  def find_or_create_discount(discount_data)
+    discount = current_period.discounts.find_or_initialize_by(name: discount_data[:name])
+    discount.img_url = discount_data[:image]
+    discount.price_old = discount_data[:old_price]
+    discount.price_new = discount_data[:new_price]
+    discount.save!
+    discount
   end
 
   def current_period
+    @current_period ||= discount_type.periods.find_or_create_by(
+      start_date: current_period_date_range.first,
+      end_date: current_period_date_range.last
+    )
+  end
+
+  def current_period_date_range
     today = Date.today
     global_starts_at = Date.parse(period[:starts_at])
     starts_at = today - ((today-global_starts_at).to_i % period[:duration_in_days]).days
