31ca61d112256e02ca7f8fb2da7083407bfeb05c
Nikita Kholin <nik.kholin@gmail.com>
2018-02-04 18:09:24 +0200

    Add endpoint for 1 shop

--- application.rb
+++ application.rb
@@ -8,16 +8,24 @@ Dir['./lib/*.rb'].each { |file| require file }
 Dir['./app/serializers/*.rb'].each { |file| require file }
 set :serializers_path, './models/serializers'
 
-get '/api/shops' do
+before do
   headers['Access-Control-Allow-Origin'] = '*'
   headers['Access-Control-Allow-Methods'] = 'GET'
   headers['Access-Control-Request-Method'] = '*'
   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+end
 
+get '/api/shops' do
   shops = Shop.includes(discount_types: { periods: :discounts }).all
   serializers = shops.map { |shop| ShopSerializer.new(shop) }
   Thread.new { parse_discounts }
   json shops: serializers
+end
+
+get '/api/shops/:slug' do
+  shop = Shop.includes(discount_types: { periods: :discounts }).find_by(slug: params[:slug])
+  Thread.new { parse_discounts }
+  json ShopSerializer.new(shop)
 end
 
 def parse_discounts
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,18 +1,34 @@
 RSpec.describe 'API', type: :controller do
-  describe 'GET /api/discounts' do
+  describe 'GET /api/shops' do
     let!(:shops) { Fabricate.times(2, :shop) }
-    before { get '/api/shops' }
+    subject! { get '/api/shops' }
 
     let(:json) { JSON.parse(last_response.body)['shops'] }
     let(:shops_count) { 2 }
-    let(:shop_discount_types_count) { 4 }
-    let(:discount_type_discounts_count) { 10 }
+    let(:discount_types_count) { 4 }
+    let(:discounts_count) { 10 }
 
     it 'should return json with all active discounts' do
       expect(last_response.status).to eq 200
       expect(json.count).to eq shops_count
-      expect(json[0]['discount_types'].count).to eq shop_discount_types_count
-      expect(json[0]['discount_types'][0]['active_period']['discounts'].count).to eq discount_type_discounts_count
+      expect(json[0]['discount_types'].count).to eq discount_types_count
+      expect(json[0]['discount_types'][0]['active_period']['discounts'].count).to eq discounts_count
+    end
+  end
+
+  describe 'GET /api/shops/:shop' do
+    let(:slug) { 'shopik' }
+    let!(:shops) { Fabricate(:shop, slug: slug) }
+    subject! { get "/api/shops/#{slug}" }
+
+    let(:json) { JSON.parse(last_response.body) }
+    let(:discount_types_count) { 4 }
+    let(:discounts_count) { 10 }
+
+    it 'should return json with all active discounts' do
+      expect(last_response.status).to eq 200
+      expect(json['discount_types'].count).to eq discount_types_count
+      expect(json['discount_types'][0]['active_period']['discounts'].count).to eq discounts_count
     end
   end
 end
