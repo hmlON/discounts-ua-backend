606d951d36fe710a12adc7e847ed8fa7bec8667b
Nikita Kholin <nik.kholin@gmail.com>
2018-02-11 19:46:32 +0200

    Fix api spec

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,18 +1,18 @@
 RSpec.describe 'API', type: :controller do
   describe 'GET /api/shops' do
-    let!(:shops) { Fabricate.times(2, :shop) }
+    let!(:shops) do
+      Shop.destroy_all
+      Fabricate.times(2, :shop)
+    end
     subject! { get '/api/shops' }
 
     let(:json) { JSON.parse(last_response.body)['shops'] }
     let(:shops_count) { 2 }
-    let(:discount_types_count) { 4 }
-    let(:discounts_count) { 10 }
 
     it 'should return json with all active discounts' do
       expect(last_response.status).to eq 200
       expect(json.count).to eq shops_count
-      expect(json[0]['discount_types'].count).to eq discount_types_count
-      expect(json[0]['discount_types'][0]['active_period']['discounts'].count).to eq discounts_count
+      expect(json[0]['slug']).to eq Shop.first.slug
     end
   end
 
