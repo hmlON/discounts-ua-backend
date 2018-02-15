425cde2c07cff843fe1e2d8520df534cdb6b13fd
Nikita Kholin <nik.kholin@gmail.com>
2018-02-15 10:01:58 +0200

    Uncomment and update xpath of silpo

--- lib/shop_parser.rb
+++ lib/shop_parser.rb
@@ -9,7 +9,7 @@ class ShopParser
     puts "parsing #{shop.name}"
     shop_data = SHOP_CONFIGS.find_shop_data(shop)
 
-    shop_data[:discount_types].each do |discount_type_slug, discount_type_data|
+    shop_data[:discount_types].map do |discount_type_slug, discount_type_data|
       discount_type = shop.discount_types.find_by(slug: discount_type_slug)
       parser = DiscountTypeParser.new(discount_type_data)
       DiscountsCreator.new(
